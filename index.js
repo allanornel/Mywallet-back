1184463c97fa6327f3d39e7f6bb079c1eba3bbe0
allanornel <allanornel@gmail.com>
2022-05-06 00:01:56 -0300

    feat: middleware and routes done

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { login, signUp } from "./controllers/authController.js";
-import { getMoviment, postMoviment } from "./controllers/movimentController.js";
-
+import authRouter from "./routes/authRouter.js";
+import movimentRouter from "./routes/movimentRouter.js";
 dotenv.config();
 
 const app = express();
 app.use(json());
 app.use(cors());
 
-app.post("/login", login);
-
-app.post("/sign-up", signUp);
-
-app.post("/moviment", postMoviment);
-
-app.get("/moviment", getMoviment);
+app.use(authRouter);
+app.use(movimentRouter);
 
 app.listen(process.env.PORTA);
